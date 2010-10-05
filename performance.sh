45593bb04c0c2f6f7742380ea0325408cbcbc300
Error323 <folkerthuizinga@gmail.com>
2010-10-05 03:56:05 +0200

    performance.sh fix

--- performance.sh
+++ performance.sh
@@ -4,30 +4,44 @@
 
 # java -jar tools/PlayGame.jar maps/map10.txt 1000 200 log.txt "./MyBot" "./submits/MyBot" &> out.txt
 
-enemies=( $1bot-* )
+enemies=( $1bot* )
 N=${#enemies[*]}
 
 wins=0
+draws=0
 losses=0
 
 process() {
 	java -jar tools/PlayGame.jar "$1" 1000 200 log.txt "./MyBot" "./$2" &> out.txt
-	echo $1 $2 `grep 'Player' out.txt`
-	result=`grep 'Player 1' out.txt`
-	if [ ! -n "$result" ]
+	echo $1 $2 `grep '!' out.txt`
+
+	win="`grep 'Player 1' out.txt`"
+	lost="`grep 'Player 2' out.txt`"
+	draw="`grep 'Draw' out.txt`"
+
+	if [ -n "$win" ]
 	then
 		(( wins++ ))
-	else
+	elif [ -n "$draw" ]
+	then
+		(( draws++ ))
+	elif [ -n "$lost" ]
+	then
 		(( losses++ ))
 	fi
+
 	rm out.txt
 }
 
 for ((i = 0; i < N; i++)); do
-	for map in maps/*; do
-		process "${map}" "${enemies[i]}"
-	done
+	if [ -x "${enemies[i]}" ]
+	then
+		for map in maps/*; do
+			process "${map}" "${enemies[i]}"
+		done
+	fi 
 done
 
 rm $1/*.txt
-echo "Won: ${wins} Lost: ${losses}"
+let total=$wins+$losses+$draws
+echo "Won: ${wins}/${total} Lost: ${losses}/${total} Draws: ${draws}/${total}"
